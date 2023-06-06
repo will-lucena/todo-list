0e3c6ec215dd2193feff0407ab385398854e8703
Will Lucena <will.lucen4@gmail.com>
2023-06-05 23:40:13 -0300

    fix checkbox not rendering

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,12 @@ import router from './router'
 const app = createApp(App)
 
 // Vuetify
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import 'vuetify/styles'
 
 const vuetify = createVuetify({
@@ -18,7 +21,14 @@ const vuetify = createVuetify({
   directives,
   theme: {
     defaultTheme: 'dark'
-  }
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 
