1589241996b68364dc2926abe74d8256e7b22d7e
Will Lucena <will.lucen4@gmail.com>
2023-07-25 22:21:46 -0300

    add set theme

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,15 @@ import ReloadPWA from '@/views/ReloadPWA.vue'
 
 const userStore = useUserStore()
 
+function setTheme() {
+  var storedTheme =
+    localStorage.getItem('theme') ||
+    (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+  if (storedTheme) document.documentElement.setAttribute('data-theme', storedTheme)
+}
+
 onMounted(async () => {
+  setTheme()
   Api.setInstance(APIs.LOCALSTORAGE)
 
   if (!currentUser) {
